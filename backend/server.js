fa41eaefd23e7d2ee6986cb4123f9eb0fc4b4e5a
karishmakumariDev <kumarikarishma311329>
2025-01-28 16:20:16 +0530

    add signup page

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express"
+import authRautes from "./routes/auth.routes.js"
 const app = express();
-app.get("/",(req,res) => {
-    res.send("server is ready");
-})
+
+
+app.use("/api/auth",authRautes);
 
 app.listen(8000, () => {
     console.log("server is runging on post 8000");
