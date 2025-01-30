7de0259afb3f9f34d6d1e585b96364550d31421f
karishmakumariDev <kumarikarishma311329>
2025-01-30 14:42:07 +0530

    mongodb connected

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,14 @@
 import express from "express"
- 
+import { login, logout, signup } from "../controllers/auth-controllers.js";
+import dotenv from "dotenv" 
 const router = express.Router();
 
-router.get("/signup",(req,res) => {
-    res.json({
-        data:"you hit the signup endpoint"
-    })
-})
+dotenv.config();//mongodb+srv://kumarikarishma311329:123456@cluster0.irmdb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+console.log(process.env.MONGO_URI)
+router.post("/signup", signup);
+router.get("/login",login);
+router.post("/logout",logout);
+
+
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import authRautes from "./routes/auth.routes.js"
 const app = express();
 
+//password = Krp0otqZ0B5sgFal
+// mongodb+srv://<db_username>:<db_password>@cluster0.irmdb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
 app.use("/api/auth",authRautes);
 
