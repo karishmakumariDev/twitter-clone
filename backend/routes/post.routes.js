1c2392c4c49008af70394eb38c7e2f514902b722
karishmakumariDev <kumarikarishma311329>
2025-02-04 17:23:06 +0530

    post controller completed

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost,deletePost,commentOnPost } from "../controllers/post-controllers.js";
+import { createPost,deletePost,commentOnPost,likeUnlikePost,getAllPosts,getlikePost , getFollowingPosts , getUserPosts} from "../controllers/post-controllers.js";
 
 const router = express.Router();
 
+router.get("/all", protectRoute, getAllPosts);
+
 router.post("/create", protectRoute, createPost);
-// router.post("/like/:id", protectRoute, likeUnlikePost);
- router.post("/comment/:id", protectRoute, commentOnPost);
- router.delete("/:id", protectRoute, deletePost);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/likes/:id", protectRoute, getlikePost);
+router.get("/user/:username", protectRoute, getUserPosts);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.delete("/:id", protectRoute, deletePost);
 
 export default router;
