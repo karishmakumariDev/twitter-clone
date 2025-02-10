1afb3c0c87274e093e15119ac438319ddb0671d4
karishmakumariDev <kumarikarishma311329>
2025-02-10 13:13:51 +0530

    3 auth handle and rout protected in clint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
