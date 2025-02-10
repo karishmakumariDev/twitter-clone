06d8ccd46bac1ba8e72b7c18d404250d8ea0d7f0
karishmakumariDev <kumarikarishma311329>
2025-02-10 10:31:46 +0530

    login page ho gya

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,12 @@ import { Route,Routes } from "react-router-dom"
 import HomePage from "./pages/home/Homepage";
 import LoginPage from "./pages/auth/LoginPage";
 import SignUpPage from "./pages/singup/SignUpPage";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
 import Sidebar from "./components/common/Sidebar";
 import RightPanel from "./components/common/RightPanel";
-import NotificationPage from "./pages/notification/NotificationPage";
-
+import { Toaster } from "react-hot-toast";
 
 
 function App() {
@@ -21,8 +23,10 @@ function App() {
           <Route path='/login' element={<LoginPage/>} />
           <Route path='/signup' element={<SignUpPage/>} />
           <Route path ='/notificationS' element={<NotificationPage/>} />
+          <Route path ='/profile/:username' element={<ProfilePage/>} />
          </Routes>
          <RightPanel/>
+         <Toaster />
       </div>
     </>
   )
